7b269117ae6f90caa24d27bb3d4195b49c774162
Anmol Pal <anmolpal@anmols-air.lan>
2023-04-02 19:32:05 -0400

    Adding Items

--- marketplaceProject/marketplaceProject/settings.py
+++ marketplaceProject/marketplaceProject/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
--- marketplaceProject/marketplaceProject/urls.py
+++ marketplaceProject/marketplaceProject/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index
-from core.views import contact
 
 #for showing images on page
 from django.conf import settings
 from django.conf.urls.static import static 
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', include('core.urls')),
     path('/items/', include('item.urls')),
-    path('contact/',contact, name='contact' ),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
